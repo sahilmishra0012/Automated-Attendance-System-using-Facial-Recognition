94d050116deda699b9487de53ec9253b23cd1ae1
Sahil Mishra <sahilmishra0012@outlook.com>
2020-03-13 16:46:43 +0530

    Production File Created

---
+++ face detection production.py
@@ -0,0 +1,26 @@
+from cv2 import cv2
+import numpy as np
+import requests
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+while(True):
+    img_res = requests.get("http://192.168.43.1:8080/shot.jpg") # Fetching requests from the IP
+    img_arr = np.array(bytearray(img_res.content), dtype = np.uint8) # Converting received contents to matrix
+
+    
+    img=cv2.imdecode(img_arr,-1) # Converting matrix to frame
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    faces = face_cascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5,minSize=(30, 30),flags = cv2.CASCADE_SCALE_IMAGE)
+    
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x-50, y-50), (x+w+50, y+h+50), (255, 255, 0), 2)
+   
+    cv2.imshow('frame', img)
+
+    if cv2.waitKey(1) & 0xFF==ord('q'):
+        break
+
+
+cv2.destroyAllWindows()
